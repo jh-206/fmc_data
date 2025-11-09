01f6a3c1f90d898d5433be267f65d5846e6b7ff1
jh-206 <jhirschi206@gmail.com>
2025-11-09 13:15:09 +0100

    split periods far apart in time so readable

--- make_ts_plots.py
+++ make_ts_plots.py
@@ -7,9 +7,18 @@ import os.path as osp
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib import transforms
 import sys
 from utils import plot_one, time_range
 
+def ts_plot(ml_data, st, ts, pi, t0, t1, batch, outfile):
+    plot_one(ml_data, st, start_time = t0, end_time = t1, title2 = f"Periods {batch}", 
+                         save_path = None, show=True)
+    for p, t in zip(pi, ts):
+        if np.isnan(p): continue
+        plt.axvline(x=t, color='black', linestyle='dotted')
+        plt.text(t, plt.ylim()[1], str(p), verticalalignment='top', horizontalalignment="right", color='black')  # Annotate
 
 if __name__ == '__main__':
 
@@ -27,11 +36,16 @@ if __name__ == '__main__':
     n_periods = 5 # number of periods per png plot file
     n_hours = 72 # hours in a period
     for st in ml_data:
+        #print("****DEBUG*****")
+        #if st == "TT255": print()
+        #else: continue
+
         d = ml_data[st]['data']
         max_period = d.st_period.max()
         for start in range(0, max_period + 1, n_periods):
             batch = list(range(start, min(start + n_periods, max_period + 1)))
             ts = [d[d['st_period'].isin([bi])].date_time.min() for bi in batch]
+            if np.all(pd.isna(ts)): continue
             pi = [d[d['st_period'].isin([bi])].st_period.min() for bi in batch]
             t0 = d[d['st_period'].isin(batch)].date_time.min()
             t1 = d[d['st_period'].isin(batch)].date_time.max()
@@ -39,15 +53,31 @@ if __name__ == '__main__':
             print(f"Running batch for station {st}")
             print(f"Start time: {t0}")
             print(f"End time: {t1}")
-            out_file = osp.join(out_dir, f"{st}_{batch[0]}_{batch[-1]}.png")
-            plot_one(ml_data, st, start_time = t0, end_time = t1, title2 = f"Periods {batch}", 
-                             save_path = None, show=True)
+            # Check for axis breaks
+            ## Some gaps between plotting periods are many months apart, 
+            ## resulting plot is unreadable. Split if possible
+            times = np.array(ts)[~pd.isna(ts)]
+            thresh = pd.Timedelta(days=90)
+            if np.any(pd.to_datetime(times).diff()[1:] > thresh):
+                for i in range(0, len(ts)):
+                    t = ts[i]
+                    if pd.isna(t): continue
+                    pp = pi[i]
+                    bb = batch[i]
+                    out_file = osp.join(out_dir, f"{st}_{bb}_{bb}.png")
+                    ts_plot(ml_data, st, [t], [pp], t, t+pd.Timedelta(hours=72), [bb], out_file)
+                    plt.savefig(out_file)
+                    plt.close()
+            else:
+                out_file = osp.join(out_dir, f"{st}_{batch[0]}_{batch[-1]}.png")
+                ts_plot(ml_data, st, ts, pi, t0, t1, batch, out_file)
+
+ 
             for p, t in zip(pi, ts):
                 if np.isnan(p): continue
                 plt.axvline(x=t, color='black', linestyle='dotted')
                 plt.text(t, plt.ylim()[1], str(p), verticalalignment='top', horizontalalignment="right", color='black')  # Annotate
-            
-            plt.savefig(out_file)  
+            plt.savefig(out_file)
             plt.close()
 
 
